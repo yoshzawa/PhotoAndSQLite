54f6c1ba74e86ba837284ff8dc48ccfed9f3e745
yoshzawa <t.yoshizawa@jc-21.jp>
2017-11-09 09:34:43 +0900

    XamlCTaskがVS2017でビルドエラーが出るのを、MobileCenterでビルドするとビルドエラーが出ない件について。あとnuGetを最新にした。ビルドエラーは変わらず。

--- photoAndSQLite/photoAndSQLite.Android/MainActivity.cs
+++ photoAndSQLite/photoAndSQLite.Android/MainActivity.cs
@@ -12,6 +12,10 @@ namespace photoAndSQLite.Droid
     [Activity(Label = "photoAndSQLite", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
