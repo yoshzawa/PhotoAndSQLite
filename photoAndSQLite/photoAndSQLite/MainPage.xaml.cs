c0d6f9ad7bb054d2a4d8cc9508b9e6ab3c06795b
yoshzawa <t.yoshizawa@jc-21.jp>
2017-11-09 13:00:41 +0900

    ページ遷移しなかったので

--- photoAndSQLite/photoAndSQLite/MainPage.xaml.cs
+++ photoAndSQLite/photoAndSQLite/MainPage.xaml.cs
@@ -16,7 +16,7 @@ namespace photoAndSQLite
 
         private void NavButton_Clicked(object sender, EventArgs e)
         {
-            new NavigationPage(new NavPage())
+            Application.Current.MainPage = new NavigationPage(new NavPage())
             {
                 BarBackgroundColor = Color.FromRgba(0.2, 0.6, 0.86, 1),
                 BarTextColor = Color.White
