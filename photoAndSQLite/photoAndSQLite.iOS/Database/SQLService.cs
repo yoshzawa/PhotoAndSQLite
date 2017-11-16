49ff171258fc5e2bee8c959d6d912a382bdaa3fb
yoshzawa <t.yoshizawa@jc-21.jp>
2017-11-16 09:52:45 +0900

    データベースのロジックを入れたけどエラーが消えてない

---
+++ photoAndSQLite/photoAndSQLite.iOS/Database/SQLService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+
+[assembly: Dependency(typeof(SQLService))]
+
+namespace photoAndSQLite.iOS.Database
+{
+    public class SQLService : photoAndSQLite.Database.ISQLService
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var personalPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var libraryPath = System.IO.Path.Combine(personalPath, "..", "Library");
+            var path = System.IO.Path.Combine(libraryPath, "MyDatabaseName");
+            return new SQLiteConnection(path);
+        }
+    }
+}
---
+++ photoAndSQLite/photoAndSQLite/Database/ISQLService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+
+// http://itblogdsi.blog.fc2.com/blog-entry-154.html
+// https://developer.xamarin.com/guides/xamarin-forms/application-fundamentals/databases/
+
+
+namespace photoAndSQLite.Database
+{
+    interface ISQLService
+    {
+        SQLiteConnection GetConnection();
+
+    }
+}
