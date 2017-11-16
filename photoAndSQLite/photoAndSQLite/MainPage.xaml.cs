4a1863df94fe8243965b47988d11b6e3013a4bc8
yoshzawa <t.yoshizawa@jc-21.jp>
2017-11-16 19:17:24 +0900

    入れなおした　https://developer.xamarin.com/recipes/android/data/databases/sqlite/

--- photoAndSQLite/photoAndSQLite/MainPage.xaml.cs
+++ photoAndSQLite/photoAndSQLite/MainPage.xaml.cs
@@ -22,5 +22,52 @@ namespace photoAndSQLite
                 BarTextColor = Color.White
             };
         }
+
+        private string createDatabase(string path)
+        {
+            try
+            {
+                var connection = new SQLiteAsyncConnection(path);
+                connection.CreateTableAsync<Person>();
+                return "Database created";
+            }
+            catch (SQLiteException ex)
+            {
+                return ex.Message;
+            }
+        }
+        private string insertUpdateData(Person data, string path)
+        {
+            try
+            {
+                var db = new SQLiteAsyncConnection(path);
+                if (db.InsertAsync(data) != 0)
+                    db.UpdateAsync(data);
+                return "Single data file inserted or updated";
+            }
+            catch (SQLiteException ex)
+            {
+                return ex.Message;
+            }
+        }
+        private int findNumberRecords(string path)
+        {
+            try
+            {
+                var db = new SQLiteAsyncConnection(path);
+                // this counts all records in the database, it can be slow depending on the size of the database
+                var count = db.ExecuteScalar<int>("SELECT Count(*) FROM Person");
+
+                // for a non-parameterless query
+                // var count = db.ExecuteScalar<int>("SELECT Count(*) FROM Person WHERE FirstName="Amy");
+
+                return count;
+            }
+            catch (SQLiteException ex)
+            {
+                return -1;
+            }
+        }
     }
 }
+
