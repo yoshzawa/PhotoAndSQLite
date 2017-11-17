2d8f1e6e081fb76a6033c019e1db80014324fcb9
yoshzawa <t.yoshizawa@jc-21.jp>
2017-11-17 16:33:35 +0900

    テストコード追加

--- photoAndSQLite/photoAndSQLite/App.xaml.cs
+++ photoAndSQLite/photoAndSQLite/App.xaml.cs
@@ -5,12 +5,20 @@ using System.Text;
 
 using Xamarin.Forms;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace photoAndSQLite
 {
     public partial class App : Application
     {
         public App()
         {
+            AppCenter.Start("ios=4767b6a1-63b4-4075-bac1-b760a033ab33;" + "uwp={Your UWP App secret here};" +
+                   "android={Your Android App secret here}",
+                   typeof(Analytics), typeof(Crashes));
+
             InitializeComponent();
 
             MainPage = new photoAndSQLite.MainPage();
