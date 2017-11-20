a39dd6c8ae0932454389ddb22198a18b581184d7
yoshzawa <t.yoshizawa@jc-21.jp>
2017-11-20 14:38:33 +0900

    nugetの設定ではまったので小休止

---
+++ photoAndSQLite/photoAndSQLite/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Realms;
+
+namespace photoAndSQLite
+{
+    public class Item : RealmObject
+    {
+        public string TimeString { get; set; }
+    }
+}
