ffd87590b0e7075bda212f5f979d30c8662bc9b4
yoshzawa <t.yoshizawa@jc-21.jp>
2017-11-30 08:38:15 +0900

    fixed 2

--- photoAndSQLite/photoAndSQLite/ByteToImageFieldConverter.cs
+++ photoAndSQLite/photoAndSQLite/ByteToImageFieldConverter.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using System.IO;
+using Xamarin.Forms;
 
 namespace photoAndSQLite
 {
